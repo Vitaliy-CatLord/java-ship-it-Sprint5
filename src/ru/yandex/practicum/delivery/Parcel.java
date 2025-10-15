fd0935d6d3962e1a736eceb7ffb439c8640d76a0
v.matkovskiy <vitaliy_matkovskiy@mail.ru>
2025-10-15 14:15:16 +0300

    base class completed

--- src/ru/yandex/practicum/delivery/Parcel.java
+++ src/ru/yandex/practicum/delivery/Parcel.java
@@ -1,5 +1,31 @@
 package ru.yandex.practicum.delivery;
 
-public class Parcel {
+public abstract class Parcel {
     //добавьте реализацию и другие необходимые классы
+    String description;
+    int weight;
+    String deliveryAddress;
+    int sendDay;
+
+
+    public Parcel(String description, int weight, String deliveryAddress, int sendDay) {
+        this.description = description;
+        this.weight = weight;
+        this.deliveryAddress = deliveryAddress;
+        this.sendDay = sendDay;
+    }
+
+    public void packageItem(Parcel parcel) {
+        System.out.println("Посылка <" + parcel.description + "> упакована");
+    }
+
+    public void deliver(Parcel parcel) {
+        System.out.println("Посылка <" + parcel.description + "> доставлена по адресу " + parcel.deliveryAddress + ".");
+    }
+
+    public void calculateDeliveryCost(Parcel parcel) {
+        System.out.println("Стоимость отправления = " + parcel.weight * parcel.getBaseCoast() + ".");
+    }
+
+    public abstract  int getBaseCoast();
 }
